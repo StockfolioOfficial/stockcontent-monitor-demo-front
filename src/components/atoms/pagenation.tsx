41c1c6d22cec5023f657e11b7d298b5198206aba
김수빈 <ksb334ksb@gmail.com>
2022-05-16 19:33:51 +0900

    fix : 코드리뷰 후 네이밍 및 기능 수정

--- src/components/atoms/pagenation.tsx
+++ src/components/atoms/pagenation.tsx
@@ -2,11 +2,6 @@ import * as React from 'react';
 import { useState, useRef } from 'react';
 import styled, { css } from 'styled-components';
 import arrow from '../../assets/images/Arrow.svg';
-// png파일 타입 지정 해주지 않았는데 에러 발생 안함 why??
-
-// 디자이너분께 질문사항
-// 버튼 disabled 관련 이슈
-// 페이지 단위 부족할 경우 이슈
 
 export interface PagenationProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement> {
@@ -115,60 +110,54 @@ export default function Pagenation(props: PagenationProps): JSX.Element {
     },
   ];
 
-  // page limit
-  // 한번에 보여주는 페이지 갯수
   const pageLimit = 10;
 
-  // page
-  // 값의 변화에따라 결국엔 clickedPage에 영향을 주어 랜더링을 하게 됨으로,
-  // 불필요한 추가 랜더링을 방지하기 위해서 useRef를 사용하여 변수의 상태값을 관리함.
-  const page = useRef(1);
+  const pageOffset = useRef(1);
 
-  // last page
-  // 버튼의 disabled관리를 하기 위한 마지막 페이지 값
   const lastPage = Math.ceil(pageList.length / pageLimit);
 
-  // 페이지 offset 값 (ex. 1페이지는 0, 2페이지부터는 pageLimit만큼 증가함)
-  // offset부터 limit만큼 잘라서 보여주기 위한 값
-  const pageOffSet = (page.current - 1) * pageLimit;
+  const pageStartPoint = (pageOffset.current - 1) * pageLimit;
 
-  // 현재 보여주고 있는 페이지의 상태값
   const [clickedPage, setClickedPage] = useState<number>(1);
 
   const goNextPage = () => {
-    page.current += 1;
-    setClickedPage(pageOffSet + pageLimit + 1);
+    pageOffset.current += 1;
+    setClickedPage(pageStartPoint + pageLimit + 1);
   };
 
   const goPrevPage = () => {
-    page.current -= 1;
-    setClickedPage(pageOffSet);
+    pageOffset.current -= 1;
+    setClickedPage(pageStartPoint);
   };
 
   return (
     <div style={{ display: 'flex', margin: '10px' }}>
-      <PagenationBtn disabled={page.current === 1} onClick={goPrevPage}>
+      <PagenationBtn disabled={pageOffset.current === 1} onClick={goPrevPage}>
         <ArrowImg src={arrow} alt="arrow" isLeft />
       </PagenationBtn>
-      {/* pageOffset값 부터 limit만큼 잘라서 map으로 뿌려준다 */}
-      {pageList.slice(pageOffSet, pageOffSet + pageLimit).map(number => {
-        if (clickedPage === number.id)
+      {pageList
+        .slice(pageStartPoint, pageStartPoint + pageLimit)
+        .map(number => {
+          if (clickedPage === number.id)
+            return (
+              <PagenationNumber key={number.id} isActive>
+                {number.id}
+              </PagenationNumber>
+            );
+
           return (
-            <PagenationNumber key={number.id} isActive>
+            <PagenationNumber
+              key={number.id}
+              onClick={() => setClickedPage(number.id)}
+            >
               {number.id}
             </PagenationNumber>
           );
-
-        return (
-          <PagenationNumber
-            key={number.id}
-            onClick={() => setClickedPage(number.id)}
-          >
-            {number.id}
-          </PagenationNumber>
-        );
-      })}
-      <PagenationBtn disabled={page.current === lastPage} onClick={goNextPage}>
+        })}
+      <PagenationBtn
+        disabled={pageOffset.current === lastPage}
+        onClick={goNextPage}
+      >
         <ArrowImg src={arrow} alt="arrow" />
       </PagenationBtn>
     </div>

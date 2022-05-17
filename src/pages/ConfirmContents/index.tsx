41c25078263d0e6365bdb7742b7c1a2b7f1cfaa4
김수빈 <ksb334ksb@gmail.com>
2022-05-17 15:58:44 +0900

    index.tsx 에 import한 pagenation 제거

--- src/pages/ConfirmContents/index.tsx
+++ src/pages/ConfirmContents/index.tsx
@@ -3,7 +3,6 @@ import useConfirmContentsParams, {
   ConfirmContentsType,
 } from '../../hooks/pathParams/useConfirmContentsParams';
 import HStackLayout from '../../components/atoms/layouts/HStackLayout';
-import Pagenation from '../../components/atoms/pagenation';
 
 export default function ConfirmContentsPage() {
   const { type, setType } = useConfirmContentsParams();
@@ -24,7 +23,6 @@ export default function ConfirmContentsPage() {
     <VStackLayout>
       <HStackLayout>{test.map(renderTypeButton)}</HStackLayout>
       <h1>{type}</h1>
-      <Pagenation />
     </VStackLayout>
   );
 }

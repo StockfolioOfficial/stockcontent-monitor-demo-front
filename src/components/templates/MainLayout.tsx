8654f2371d41defcf9fcfa1fff1f8ef9073e94d7
songyi-p <songyi7091@gmail.com>
2022-05-20 17:18:19 +0900

    feat: MainLayout 리뷰 후 수정 : 중복 import 제거 및 에러 처리 #30

--- src/components/templates/MainLayout.tsx
+++ src/components/templates/MainLayout.tsx
@@ -1,22 +1,20 @@
-import styled from 'styled-components';
-import BaseLayoutProps from '../types/BaseLayoutProps';
-import * as React from 'react';
-import MainItem from '../molecules/MainItem';
-import { MainDataProps } from '../types/CommonDataProps';
-import Pagenation from '../../components/atoms/Pagenation';
-import { ConfirmContentsType } from '../../hooks/pathParams/useConfirmContentsParams';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
-
+import styled from 'styled-components';
+import MainItem from '../molecules/MainItem';
+import Pagenation from '../../components/atoms/Pagenation';
+import BaseLayoutProps from '../types/BaseLayoutProps';
+import { MainDataProps } from '../types/CommonDataProps';
+import { ConfirmContentsType } from '../../hooks/pathParams/useConfirmContentsParams';
 export interface MainItemLayoutProps extends BaseLayoutProps {
   type: ConfirmContentsType;
 }
 
 const MainItemStyled = styled.div`
-  width: 1200px;
   display: flex;
-  gap: 60px 8px;
   flex-wrap: wrap;
+  gap: 60px 8px;
+  width: 1200px;
   margin: auto;
   padding: 45px 0 86px 0;
 `;
@@ -40,10 +38,23 @@ export default function MainLayout({ type }: MainItemLayoutProps) {
     },
   ]);
 
+  //mainItemList 데이터 fetch
   useEffect(() => {
     axios
       .get('http://localhost:3000/data/MainItemList.json')
-      .then(res => setMainItemList(res.data));
+      .then(res => setMainItemList(res.data))
+      .catch(err => {
+        if (err.response) {
+          console.log(err.response.data);
+          console.log(err.response.status);
+          console.log(err.response.headers);
+        } else if (err.request) {
+          console.log(err.request);
+        } else {
+          console.log('Error', err.message);
+        }
+        console.log(err.config);
+      });
   }, []);
 
   return (
